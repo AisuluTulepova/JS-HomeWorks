2d41d27193136b6b3d28e6c81047b508ecc1a9ea
Aisulu <aisulutulepova4@gmail.com>
2024-11-19 12:13:23 +0500

    added new commit

---
+++ home work JS 1/JavaScript/comment.js
@@ -0,0 +1,16 @@
+// console.log("JavaScript-тегі алғашқы үй тапсырмасы");
+console.log("Сәлем, JavaScript әлемі");
+console.log();
+/*  "Бүгінгі тақырып"
+console.log("екі таңбалы сандарды қосу көбейту амалы");
+console.log(10*12);
+console.log(56+23);
+console.log('20*2' + "немесе"+ '20+20');
+console.log("он бес қосу бес" + '15+5');*/
+console.log("Менің атым: Айсұлу");
+
+
+
+
+
+
