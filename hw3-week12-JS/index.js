aa591567ec27ab40f15fb433e9e0a77705bedb62
Aisulu <aisulutulepova4@gmail.com>
2024-11-22 21:01:00 +0500

    Hw-4 week12-JS

--- hw3-week12-JS/index.js
+++ hw3-week12-JS/index.js
@@ -1,28 +1,52 @@
-let Euro = 0;
+// let Euro = 0;
 
-while (true) {
-    let aqsha = prompt("Ақша сомасын енгізіңіз (немесе 'stop' деп жазып, тоқтату үшін):");
+// while (true) {
+//     let aqsha = prompt("Ақша сомасын енгізіңіз (немесе 'stop' деп жазып, тоқтату үшін):");
     
-    if (aqsha.toLowerCase() === 'stop') {
-        break;
-    }
-    let valut = prompt("Валютаны енгізіңіз (USD, KZT, EUR):");
+//     if (aqsha.toLowerCase() === 'stop') {
+//         break;
+//     }
+//     let valut = prompt("Валютаны енгізіңіз (USD, KZT, EUR):");
 
 
-    switch (valut.toUpperCase()) {
-        case 'USD':
-            Euro += parseFloat(aqsha) * 0.85; 
-            break;
-        case 'KZT':
-            Euro += parseFloat(aqsha) * 0.002; 
-            break;
-        case 'EUR':
-            Euro += parseFloat(aqsha); 
-            break;
-        default:
-            alert("Қате валютаны енгіздіңіз. Қайталап көріңіз.");
-            continue;
-    }
-}
+//     switch (valut.toUpperCase()) {
+//         case 'USD':
+//             Euro += parseFloat(aqsha) * 0.85; 
+//             break;
+//         case 'KZT':
+//             Euro += parseFloat(aqsha) * 0.002; 
+//             break;
+//         case 'EUR':
+//             Euro += parseFloat(aqsha); 
+//             break;
+//         default:
+//             alert("Қате валютаны енгіздіңіз. Қайталап көріңіз.");
+//             continue;
+//     }
+// }
 
-alert("Барлық соманың евродағы қосындысы: " + Euro + " EUR");
+// alert("Барлық соманың евродағы қосындысы: " + Euro + " EUR");
+
+
+// let san1 = + prompt('Жасыңызды жазыңыз:')
+// function zhas(){
+//     if(san1<0){
+//         alert('Олай болу мүмкін емес!!!')
+//   } else  if (san1<18){
+//         alert('Cіз кәмелетке толмағансыз') 
+//   }else if(san1>=18 && san1 <=120){
+          
+//         alert('Сіз ересексіз')
+//   }else {
+//         alert('Error')
+//   } 
+
+// }
+   
+// zhas()
+
+
+
+
+
+
