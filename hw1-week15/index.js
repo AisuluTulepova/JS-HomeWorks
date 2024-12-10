a0dadbc05822d9cf236d4fbb5c4e7f826306af23
Aisulu <aisulutulepova4@gmail.com>
2024-12-10 22:55:41 +0500

    hw1 week15

---
+++ hw1-week15/index.js
@@ -0,0 +1,13 @@
+// 1 Тапсырма:
+let taqyryp2=document.querySelector('#taqyryp1')
+taqyryp2.textContent="HW: Introduction to DOM"
+
+// 2 Тапсырма:
+let FIO2 = document.getElementById("FIO1")
+FIO2.textContent="Айсулу Тулепова:"
+
+// 3 Тапсырма:
+let hobby=document.getElementsByClassName("tizim")
+        hobby[0].innerHTML = '<b>Coding</b>';
+        hobby[1].innerHTML = '<b>Student</b>';
+        hobby[2].innerHTML = '<b>Watching movies</b>';
